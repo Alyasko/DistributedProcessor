200cd70fcfbb4632fde7312d54c8fe8fa242ede7
Alexander <yasko.alexander@gmail.com>
2017-03-18 14:00:52 +0200

    Added OpenMP and MPI support.

--- DistributedProcessor/DistributedProcessor/Main.cpp
+++ DistributedProcessor/DistributedProcessor/Main.cpp
@@ -1,10 +1,15 @@
 #include "stdio.h"
 #include "Worker.h"
+#include "mpi.h"
 
-int main()
+int main(int argc, char** argv)
 {
+	MPI_Init(&argc, &argv);
+
 	Worker *worker = new Worker();
 	worker->Run();
+
+	MPI_Finalize();
 
 	return 0;
 }
