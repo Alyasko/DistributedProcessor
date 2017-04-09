07897ca4f4ff0e1714612562919212c8249b8a31
Alexander <yasko.alexander@hotmail.com>
2017-04-09 17:24:23 +0300

    Modified test script.

--- Docs/Scripts/start short.cmd
+++ Docs/Scripts/start short.cmd
@@ -1,8 +1,32 @@
 echo off 
 FOR %%n IN (1,2,4,8,10,12,16) DO (
-	echo processes: %%n >> out.txt
+	echo processes: %%n >> out_test_1.txt
 	FOR %%t IN (1,2,4,8,10,12,16) DO (
-		mpiexec -n %%n DistributedProcessor 50000000 %%t >> out.txt
+		mpiexec -n %%n "../../DistributedProcessor/Debug/DistributedProcessor.exe" 50000000 %%t >> out_test_1.txt
+		timeout 1
+	)
+)
+
+FOR %%n IN (1,2,4,8,10,12,16) DO (
+	echo processes: %%n >> out_test_2.txt
+	FOR %%t IN (1,2,4,8,10,12,16) DO (
+		mpiexec -n %%n "../../DistributedProcessor/Debug/DistributedProcessor.exe" 50000000 %%t >> out_test_2.txt
+		timeout 1
+	)
+)
+
+FOR %%n IN (1,2,4,8,10,12,16) DO (
+	echo processes: %%n >> out_test_3.txt
+	FOR %%t IN (1,2,4,8,10,12,16) DO (
+		mpiexec -n %%n "../../DistributedProcessor/Debug/DistributedProcessor.exe" 50000000 %%t >> out_test_3.txt
+		timeout 1
+	)
+)
+
+FOR %%n IN (1,2,4,8,10,12,16) DO (
+	echo processes: %%n >> out_test_4.txt
+	FOR %%t IN (1,2,4,8,10,12,16) DO (
+		mpiexec -n %%n "../../DistributedProcessor/Debug/DistributedProcessor.exe" 50000000 %%t >> out_test_4.txt
 		timeout 1
 	)
 )
