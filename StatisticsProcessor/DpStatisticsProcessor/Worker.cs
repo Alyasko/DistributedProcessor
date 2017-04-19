5f5f35520b776e55ec350fa484b8449caf88ce89
Alexander <yasko.alexander@hotmail.com>
2017-04-19 21:01:55 +0300

    Added StatisticsProcessor project.

---
+++ StatisticsProcessor/DpStatisticsProcessor/Worker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StatisticsProcessorBl;
+
+namespace DpStatisticsProcessor
+{
+    public class Worker
+    {
+        private readonly StatisticsProcessor _statisticsProcessor;
+
+        public Worker()
+        {
+            _statisticsProcessor = new StatisticsProcessor();
+        }
+
+        public void Run()
+        {
+            _statisticsProcessor.InputFileName = GetFileNameFromArgs(FileNameType.InputFileName);
+            _statisticsProcessor.OutputFileName = GetFileNameFromArgs(FileNameType.OutputFileName);
+        }
+
+        private String GetFileNameFromArgs(FileNameType fileNameType)
+        {
+            String fileName = "";
+
+            if (Arguments.Length == 1)
+            {
+                throw new ArgumentException("Insufficient number of command line arguments.");
+            }
+
+            switch (fileNameType)
+            {
+                case FileNameType.InputFileName:
+                    fileName = GetFileNameSafe(1);
+                    break;
+                case FileNameType.OutputFileName:
+                    fileName = GetFileNameSafe(2);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(fileNameType), fileNameType, null);
+            }
+
+            return fileName;
+        }
+
+        private String GetFileNameSafe(int argIndex)
+        {
+            if (argIndex < Arguments.Length)
+            {
+                if (File.Exists(Arguments[argIndex]))
+                {
+                    return Arguments[argIndex];
+                }
+                else
+                {
+                    throw new FileNotFoundException("File does not exist.");
+                }
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Not element with such index in command line arguments."); 
+            }
+        }
+
+        public string[] Arguments { get; set; }
+    }
+}
---
+++ StatisticsProcessor/StatisticsProcessorBl/StatisticsProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatisticsProcessorBl
+{
+    public class StatisticsProcessor
+    {
+        public String InputFileName { get; set; }
+        public String OutputFileName { get; set; }
+    }
+}
